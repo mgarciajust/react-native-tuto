97780cc535ea7d9414a05d2b439442a60b5cd13b
calypso-mc <malinela@gmail.com>
2024-02-11 17:41:04 +0100

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+//Aquí inicializamos nuestra aplicación
+const Main = () => {
+    return (
+        <View>
+            <Text>
+                Rate Repository Application
+            </Text>
+        </View>
+    )
+}
+
+export default Main
