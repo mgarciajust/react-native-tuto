96dee66a530b60e4433a73e3162e79900ca11ce7
calypso-mc <malinela@gmail.com>
2024-02-12 20:52:44 +0100

    styles

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,5 +1,20 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, Image, StyleSheet } from "react-native"
 import React from "react"
+
+const RepositoryStats = (props) => {
+    return(
+        <View style={styles.flex}>
+            <View>
+                <Text style={styles.bold}>Languaje:</Text>
+                <Text style={styles.center}>{props.languaje}</Text>
+            </View>
+            <View>
+                <Text style={styles.bold}>Forks Counts: </Text>
+                <Text style={styles.center}>{props.forksCount}</Text>
+            </View>
+        </View>
+    )
+}
 
 //Aquí estilo los items de mi lista
 //Le paso cada item con props (parámetros)
@@ -8,15 +23,15 @@ const RepositoryItem = (props) => (
         key={props.id}
         style={styles.container}>
 
-        <Text
-            style={styles.strong}>
-            {props.id}
-        </Text>
-        <Text>{props.fullname}</Text>
-        <Text>{props.description}</Text>
-        <Text>{props.languaje}</Text>
-        <Text>{props.forksCount}</Text>
-        <Text>{props.url}</Text>
+        <View style={styles.left}>
+            <Image style={styles.image} source={{uri: props.url}}/>
+            <Text style={styles.strong}>{props.fullname}</Text>
+        </View>
+    
+        <Text style={styles.text}>{props.description}</Text>
+        <Text style={styles.button}>url</Text>
+        <Text style={styles.blue}>{props.url}</Text>
+        <RepositoryStats {...props}/>
     </View>
 
 )
@@ -26,13 +41,61 @@ const styles = StyleSheet.create({
     container: {
         padding: 20,
         paddingBottom: 10,
-        paddingTop: 5
+        paddingTop: 5,
+        backgroundColor: "#f5f5f5",
+        borderRadius: 10,
+        marginBottom: 10,
+        elevation: 2,
     },
     strong: {
         fontWeight: "bold",
         marginBottom: 5,
         color: "blue"
-    },    
+    },
+    text: {
+        fontSize: 16,
+        marginBottom: 5,
+        color: "#333333"
+    },
+    bold: {
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    blue: {
+        color: "blue"
+    },
+    small: {
+        fontSize: 16,
+        marginBottom: 5,
+        color: "#5c5c5c"
+    },
+    flex: {
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },
+    left: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center"
+    },
+    center: {
+        textAlign: "center"
+    },
+    button: {
+        color: "white",
+        backgroundColor: "blue",
+        borderRadius: 4,
+        overflow: "hidden",
+        alignSelf: "flex-start",
+        padding: 5
+    },
+    image: {
+        width: 48,
+        height: 48,
+        borderRadius: 4,
+        marginRight: 10
+    }
+
 })
 
 export default RepositoryItem
